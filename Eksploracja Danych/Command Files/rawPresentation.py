fc9fcdc4282acb31ec28a2ba66d162feb008de90
Sylwester Dawida <Sylwo103@interia.pl>
2019-12-12 17:26:48 +0100

    ead update 2

--- Eksploracja Danych/Command Files/rawPresentation.py
+++ Eksploracja Danych/Command Files/rawPresentation.py
@@ -7,6 +7,7 @@ with pd.option_context('display.max_columns', None):  # more options can be spec
     #df.info()
     # df().count(axis='columns')
     # with pd.option_context('display.max_columns', None):  # more options can be specified also
+    #grupowanie po rodzaju muzyki
     grouped = df.groupby('genre')
     result = pd.DataFrame(columns=("genre"
                                    , "topTen", "tracks number"))
@@ -14,9 +15,12 @@ with pd.option_context('display.max_columns', None):  # more options can be spec
     t = tk(total=len(grouped))  # Initialise
     for name, group in grouped:
         t.update()
+        #liczenie ile razy utwory z danego rodzaju muzyki były top 10
         res = group[group.iloc[:, 7:] <= 10].count()
         res.rename(name)
+        #suma
         final = res.sum()
+        #dodatnie wiersza do result z nazwą grupy wynikiem oraz liczbą tracków
         result.loc[len(result)] = [name, final, len(group)]
         # print(group)
 
@@ -25,10 +29,12 @@ with pd.option_context('display.max_columns', None):  # more options can be spec
     print("*********Gatunek muzyki z najczęstszym top 10*********")
     print(result[result.topTen == result.topTen.max()])
 
+#grupowanie po czasie utworów
     grouped = df.groupby('time')
     result = pd.DataFrame(columns=("time"
                                    , "topTen", "tracks number"))
     # print(grouped)
+    #analogicznie do poprzedniego kodu
     t = tk(total=len(grouped))  # Initialise
     for name, group in grouped:
         t.update()
@@ -43,3 +49,5 @@ with pd.option_context('display.max_columns', None):  # more options can be spec
     print(result[result.topTen == result.topTen.max()])
     print("*********Grupa z najrzadszym top 10*********")
     print(result[result.topTen == result.topTen.min()])
+    #najcześciej top 10 były utworu z czasem trwania 3:52
+    #najrzadszych  w top 10 było 124
